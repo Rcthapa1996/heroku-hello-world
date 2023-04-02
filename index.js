b52f086f45e9d56f78d0831287e2f91df5be9595
Rcthapa1996 <Rcthapa1996@gmail.com>
2023-04-02 18:18:20 +0545

    update it

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var http = require('http')
+var http = require("http");
 
-http.createServer(function (request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"})
-	response.end("Hello World\n")
-}).listen(process.env.PORT)
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World\n");
+  })
+  .listen(process.env.PORT);
